8adb68df5e70800d7b033b65358c6060862610cc
khemsoth <kevhemsoth@gmail.com>
2021-10-17 08:10:20 -0400

    Add project files.

---
+++ number-guessing/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace number_guessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Number Guessing Program. Please select a number between 1-100.");
+        }
+    }
+}
